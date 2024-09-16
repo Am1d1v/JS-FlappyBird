9213ba3539d47c056eeb7a934134227e599a13c7
Am1d1v <dimadiman1adima@gmail.com>
2024-09-16 11:56:43 +0300

    Creating Phaser App

--- src/index.js
+++ src/index.js
@@ -1,28 +1,29 @@
-
 import Phaser from "phaser";
 
+// Game Configuration
 const config = {
   type: Phaser.AUTO,
-  width: 800,
+  width: 900,
   height: 600,
   physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 200 }
-    }
+    default: 'arcade'
   },
   scene: {
-    preload: preload,
-    create: create
+    preload,
+    create,
+    update
   }
 };
 
+// Scene Preload. Loading assets
+function preload(){
+
+}
+
+// Scene Create
+function create(){
+
+}
+
+
 new Phaser.Game(config);
-
-function preload () {
-  this.load.image('sky', 'assets/sky.png');
-}
-
-function create () {
-  this.add.image(400, 300, 'sky');
-}
