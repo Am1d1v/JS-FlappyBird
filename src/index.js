4de1d87a926e10450455b991eb775b89967b4f1b
Am1d1v <dimadiman1adima@gmail.com>
2024-09-17 15:26:29 +0300

    Restart player's position

--- src/index.js
+++ src/index.js
@@ -24,8 +24,11 @@ const config = {
 const VELOCITY = 200;  
 
 let bird;
-let totalDelta = null;
 let flapVelocity = 300;
+const initialBirdPosition = {
+  x: config.width * 0.1,
+  y: config.height * 0.5
+}
 
 // Scene Preload. Loading assets
 function preload(){
@@ -39,10 +42,10 @@ function preload(){
 // Scene Create
 function create(){
   // Add Image to the Scene
-  this.add.image(0, 0, 'sky').setOrigin(0, 0);
+  this.add.image(0, 0, 'sky').setOrigin(0);
 
   // Add Bird sprite
-  bird = this.physics.add.sprite(config.width * 0.1, config.height * 0.5, 'bird').setOrigin(0);
+  bird = this.physics.add.sprite(initialBirdPosition.x, initialBirdPosition.y, 'bird').setOrigin(0);
 
   // Flap the bird
   this.input.keyboard.on('keydown-SPACE', flap)
@@ -55,13 +58,19 @@ function update(time, delta){
 
   // Restart the game if the bird cross Y axis top/bottom borders of the Scene
   if(bird.y > config.height || bird.y < 0){
-    console.log(bird.body);
+    restartPlayerPosition();
   }
 }
 
 // Flap. Move up the bird
 function flap(){
   bird.body.velocity.y = -flapVelocity;
+}
+
+// Restart player position
+function restartPlayerPosition(){
+  bird.x = initialBirdPosition.x;
+  bird.y = initialBirdPosition.y;
 }
 
 
