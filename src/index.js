302a8532c5a38ba7f84ff9d32cee512923fed048
Am1d1v <dimadiman1adima@gmail.com>
2024-09-17 14:46:33 +0300

    Flapping Feature

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const config = {
     default: 'arcade',
     arcade: {
       gravity: {
-        //y: 20,
+        y: 200,
       },
       debug: true
     }
@@ -42,29 +42,18 @@ function create(){
 
   // Add Bird sprite
   bird = this.physics.add.sprite(config.width * 0.1, config.height * 0.5, 'bird').setOrigin(0);
-  bird.body.velocity.x = VELOCITY;
+
+  this.input.keyboard.on('keydown-SPACE', () => {
+    console.log('W');
+  })
+
+  this.input.on('pointerdown', () => {
+    console.log('click');
+  });
 }
 
 // Scene update. Application rerendering
 function update(time, delta){
-
-  totalDelta += delta;
-
-  if(totalDelta >= 1000){
-    //console.log(bird.body.velocity.y);
-
-    // Reset totalDelta value to 0 after 1 second
-    totalDelta = 0;
-  };
-
-  // If bird X position is same or larger than width of canvas change bird X axis movement direction
-  // If bird X position is smaller or equal to zero change bird X axis movement direction
-  if(bird.x >= config.width - bird.width){
-    bird.body.velocity.x = -VELOCITY;
-  } else if(bird.x <= 0){
-    bird.body.velocity.x = VELOCITY;
-  };
-
   
   
 }
