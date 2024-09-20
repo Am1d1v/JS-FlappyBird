90b9c3a27245a136c1135b5998d5eb5c80927eb3
Am1d1v <dimadiman1adima@gmail.com>
2024-09-20 14:35:28 +0300

    PlayScene Refactoring

--- src/index.js
+++ src/index.js
@@ -31,123 +31,12 @@ const VELOCITY = 200;
 const initialBirdPosition = {
   x: config.width * 0.1,
   y: config.height * 0.5
-};
-const pipeVerticalDistanceRange = [150, 250];
-const pipeHorizontalDistanceRange = [200, 450];
-const PIPES_TO_RENDER = 3;
+}
 
-let bird;
-let pipes = null;
 let flapVelocity = 300;
-let pipeHorizontalDistance = 0;
 
 
-// Scene Preload. Loading assets
-function preload(){
-  // Load sky background image
-  this.load.image('sky', 'assets/sky.png');
 
-  // Load Bird image
-  this.load.image('bird', 'assets/bird.png');
-
-  // Load Pipe image
-  this.load.image('pipe', 'assets/pipe.png')
-}
-
-// Scene Create
-function create(){
-  // Add Image to the Scene
-  this.add.image(0, 0, 'sky').setOrigin(0);
-
-  // Add Bird sprite
-  bird = this.physics.add.sprite(initialBirdPosition.x, initialBirdPosition.y, 'bird').setOrigin(0);
-  // Bird's gravity
-  bird.body.gravity.y = 300;
-  
-  pipes = this.physics.add.group();
-
-  // Pipes generation
-  for(let i = 0; i < PIPES_TO_RENDER; i++){
-
-    // Add Pipes sprite
-    const upperpipe = pipes.create(0, 0, 'pipe').setOrigin(0, 1);
-    const lowerpipe = pipes.create(0, 0, 'pipe').setOrigin(0, 0);
-
-    placePipe(upperpipe, lowerpipe);
-  };
-  
-  // Pipes X axis velocity
-  pipes.setVelocityX(-200);
-
-  // Flap the bird
-  this.input.keyboard.on('keydown-SPACE', flap)
-  this.input.on('pointerdown', flap);
-
-}
-
-// Scene update. Application rerendering
-function update(time, delta){
-  
-  // Restart the game if the bird cross Y axis top/bottom borders of the Scene
-  if(bird.y > config.height || bird.y < 0){
-    restartPlayerPosition();
-  }
-
-  recyclePipes();
-}
-
-// Flap. Move up the bird
-function flap(){
-  bird.body.velocity.y = -flapVelocity;
-};
-
-// Restart player position
-function restartPlayerPosition(){
-  bird.x = initialBirdPosition.x;
-  bird.y = initialBirdPosition.y;
-  bird.body.velocity.y = 0;
-};
-
-// Pipes placing
-function placePipe(uPipe, lPipe){
-    const mostRightX = getMostRightPipe();
-    let pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalDistanceRange);
-    let pipeVerticalPosition = Phaser.Math.Between(0 + 20, config.height - 20 - pipeVerticalDistance);
-    const pipeHorizontalDistance = Phaser.Math.Between(...pipeHorizontalDistanceRange); 
-
-    uPipe.x = mostRightX + pipeHorizontalDistance;
-    uPipe.y = pipeVerticalPosition;
-
-    lPipe.x = uPipe.x;
-    lPipe.y = uPipe.y + pipeVerticalDistance;
-};
-
-// Pipes Recycling
-function recyclePipes(){
-  let pipesArray = [];
-
-  pipes.getChildren().forEach((pipe) => {
-
-    if(pipe.getBounds().right <= 0){
-      pipesArray.push(pipe);
-      if(pipesArray.length === 2){
-        placePipe(...pipesArray);
-      }
-    }
-
-  });
-
-};
-
-function getMostRightPipe(){
-  let mostRightX = 0;
-
-  pipes.getChildren().forEach((pipe) => {
-    mostRightX = Math.max(pipe.x, mostRightX);
-  });
-
-  return mostRightX;
-}
 
 
 new Phaser.Game(config);
--- src/scenes/PlayScene.js
+++ src/scenes/PlayScene.js
@@ -1,22 +1,36 @@
 import Phaser from "phaser";
 
 
+const PIPES_TO_RENDER = 3;
+
+
 class PlayScene extends Phaser.Scene{
     constructor(config){
         super('PlayScene');
 
         this.config = config;
         this.bird = null;
+        this.pipes = null;
+        this.pipeVerticalDistanceRange = [150, 250];
+        this.pipeHorizontalDistanceRange = [200, 450];
+
+
+let pipeHorizontalDistance = 0;
     }
 
+    // Scene Preload. Loading assets
     preload(){
         // Load sky background image
         this.load.image('sky', 'assets/sky.png');
 
         // Load Bird image
         this.load.image('bird', 'assets/bird.png');
-    }
 
+        // Load Pipe image
+        this.load.image('pipe', 'assets/pipe.png');
+    };
+
+    // Scene Create
     create(){
         // Add Image to the Scene
         this.add.image(0, 0, 'sky').setOrigin(0);
@@ -26,11 +40,90 @@ class PlayScene extends Phaser.Scene{
 
         // Bird's gravity
         this.bird.body.gravity.y = 300;
-    }
 
+        this.pipes = this.physics.add.group();
+
+        // Pipes generation
+        for(let i = 0; i < PIPES_TO_RENDER; i++){
+        
+          // Add Pipes sprite
+          const upperpipe = this.pipes.create(0, 0, 'pipe').setOrigin(0, 1);
+          const lowerpipe = this.pipes.create(0, 0, 'pipe').setOrigin(0, 0);
+        
+          this.placePipe(upperpipe, lowerpipe);
+        };
+
+        // Pipes X axis velocity
+        this.pipes.setVelocityX(-200);
+    
+        // Flap the bird
+        this.input.keyboard.on('keydown-SPACE', this.flap)
+        this.input.on('pointerdown', this.flap);
+    };
+
+    // Scene update. Application rerendering
     update(){
+        // Restart the game if the bird cross Y axis top/bottom borders of the Scene
+        if(this.bird.y > this.config.height || this,bird.y < 0){
+          restartPlayerPosition();
+        }
 
-    }
+        this.recyclePipes();
+    };
+
+    // Flap. Move up the bird
+    flap(){
+        this.bird.body.velocity.y = -flapVelocity;
+    };
+
+    // Restart player position
+    restartPlayerPosition(){
+        this.bird.x = initialBirdPosition.x;
+        this.bird.y = initialBirdPosition.y;
+        this.bird.body.velocity.y = 0;
+    };
+
+    // Pipes placing
+    placePipe(uPipe, lPipe){
+        const mostRightX = getMostRightPipe();
+        let pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalDistanceRange);
+        let pipeVerticalPosition = Phaser.Math.Between(0 + 20, config.height - 20 - pipeVerticalDistance);
+        const pipeHorizontalDistance = Phaser.Math.Between(...pipeHorizontalDistanceRange); 
+
+        uPipe.x = mostRightX + pipeHorizontalDistance;
+        uPipe.y = pipeVerticalPosition;
+
+        lPipe.x = uPipe.x;
+        lPipe.y = uPipe.y + pipeVerticalDistance;
+    };
+
+    // Pipes Recycling
+    recyclePipes(){
+      let pipesArray = [];
+    
+      this.pipes.getChildren().forEach((pipe) => {
+    
+        if(pipe.getBounds().right <= 0){
+          pipesArray.push(pipe);
+          if(pipesArray.length === 2){
+            placePipe(...pipesArray);
+          }
+        }
+    
+        });
+    
+    };
+
+    getMostRightPipe(){
+        let mostRightX = 0;
+      
+        pipes.getChildren().forEach((pipe) => {
+          mostRightX = Math.max(pipe.x, mostRightX);
+        });
+      
+        return mostRightX;
+    };
+
 }
 
 export default PlayScene;
